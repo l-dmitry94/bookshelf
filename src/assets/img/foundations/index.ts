51952b313576c83c629a50e5938a02b6a4b93962
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2026-09-08 13:25:23 +0300

    added foundations

---
+++ src/assets/img/foundations/index.ts
@@ -0,0 +1,41 @@
+import foundation1 from './foundation-1.png';
+import foundation1_2x from './foundation-1@2x.png';
+import foundation2 from './foundation-2.png';
+import foundation2_2x from './foundation-2@2x.png';
+import foundation3 from './foundation-3.png';
+import foundation3_2x from './foundation-3@2x.png';
+import foundation4 from './foundation-4.png';
+import foundation4_2x from './foundation-4@2x.png';
+import foundation5 from './foundation-5.png';
+import foundation5_2x from './foundation-5@2x.png';
+import foundation6 from './foundation-6.png';
+import foundation6_2x from './foundation-6@2x.png';
+import foundation7 from './foundation-7.png';
+import foundation7_2x from './foundation-7@2x.png';
+import foundation8 from './foundation-8.png';
+import foundation8_2x from './foundation-8@2x.png';
+import foundation9 from './foundation-9.png';
+import foundation9_2x from './foundation-9@2x.png';
+
+const foundationsImages = {
+    foundation1,
+    foundation1_2x,
+    foundation2,
+    foundation2_2x,
+    foundation3,
+    foundation3_2x,
+    foundation4,
+    foundation4_2x,
+    foundation5,
+    foundation5_2x,
+    foundation6,
+    foundation6_2x,
+    foundation7,
+    foundation7_2x,
+    foundation8,
+    foundation8_2x,
+    foundation9,
+    foundation9_2x,
+};
+
+export default foundationsImages;
